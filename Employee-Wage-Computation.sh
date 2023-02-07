f38e141e3ed5b28072541dd6d55c069ec4bda1c1
wanarekapil96@ <wanarekapil96@gmail.com>
2023-02-07 20:16:32 +0530

    Added :: It Will Show The Employee Daily Working

--- Employee-Wage-Computation.sh
+++ Employee-Wage-Computation.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
-PRESENT=1;
-isPresent=$((RANDOM%2));
-
-if [ $isPresent -eq $PRESENT ]
+#attendence check
+attendence=$(( RANDOM % 2 ))
+if(( attendence == 0 ))
 then
-	echo "Employee is Present";
+    echo "Employee is absent"
 else
-	echo "Employee is Absent";
+    echo "Employee is present"
 fi
+
+#calculation of daily employee wage
+wage_per_hour=20
+no_of_hours=8
+echo "The Daily Employee Wage is $(( attendence * wage_per_hour * no_of_hours ))"
